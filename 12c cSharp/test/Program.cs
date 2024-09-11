04e23fe768165ad17cf415053591a85c68eeb6b5
Patrik András Budinszki <2021124@dszcberegszaszi.hu>
2024-09-11 08:53:33 +0200

    asd

--- 12c cSharp/test/Program.cs
+++ 12c cSharp/test/Program.cs
@@ -10,7 +10,7 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sdadassdf");
+            Console.WriteLine("asdasdas");
         }
     }
 }
