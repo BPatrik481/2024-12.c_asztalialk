165df1e066ea3a6738fdc9853d335b9c1d8056e5
Patrik András Budinszki <2021124@dszcberegszaszi.hu>
2024-10-02 07:31:09 +0200

    jav

--- 12c cSharp/tobbelem2/Program.cs
+++ 12c cSharp/tobbelem2/Program.cs
@@ -3,29 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-/*Írjunk olyan programot, amely addig kér
+/*
+* Írjunk olyan programot, amely addig kér
 be egész számokat a billentyűzetről, amíg azok összege meg nem haladja a 100-at. A
 beolvasás végén írjuk ki azt, hogy a bekért számok közül hány volt páros, és hány volt
-páratlan.*/
-
-namespace tobbelem2
+páratlan.
+*/
+namespace TobbElem
 {
     class TobbElemOsztaly
     {
-        //osztályváltozók
+        // Osztályváltozók
         private int szam;
         private bool isParos;
-        //konstruktor
+
+        // Konstruktor
         public TobbElemOsztaly()
         {
-           this.szam = 0;
-           this.isParos = false;
+            this.szam = 0;
+            this.isParos = false;
         }
 
-        public void Getszam(int p)
+        public void GetSzam(int p)
         {
-           this.szam = p;
+            this.szam = p;
         }
 
         public void SetSzamTipus()
@@ -37,41 +38,45 @@ namespace tobbelem2
         {
             return this.isParos;
         }
+
+
+
     }
     class Program
     {
         static void Main(string[] args)
         {
-            //osztályváltozók
+            // osztályváltozók
             string valasz = String.Empty;
             int paros = 0,
-                szam=0,
+                szam = 0,
             paratlan = 0;
 
-            //Példányosítjuk az osztályt
-            //másnéven objektumot hozunk létre az osztálynak
 
+            // Példányosítjuk az osztályt,
+            // más néven objektumot hozunk létre az osztályból
             TobbElemOsztaly a = new TobbElemOsztaly();
 
-            //Szám megadása
+            // Szám megadása
             while (paros + paratlan < 100)
             {
-                Console.WriteLine("adj meg egy egész számot:");
+                Console.WriteLine("Adj meg egy egész számot!");
                 szam = Convert.ToInt32(Console.ReadLine());
-                a.Getszam(szam);
+                a.GetSzam(szam);
                 a.SetSzamTipus();
                 if (a.outSzamTipus())
                 {
-                    valasz = "paros";
-                    paros+=szam;
+                    valasz = "páros";
+                    paros += szam;
                 }
                 else
                 {
-                    valasz = "paratlan";
-                    paratlan+=szam;
+                    valasz = "páratlan";
+                    paratlan += szam;
                 }
-                Console.WriteLine($"A szám tipusa: {valasz}, eddig {paros + paratlan} számot adott meg.");
+                Console.WriteLine($"A szám típusa: {valasz}, eddig a számok összege: {paros + paratlan}");
                 Console.ReadKey();
+
             }
         }
     }
